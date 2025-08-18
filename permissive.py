7a6a0de6a0c23b73edff7e0a3f5b019606e1afcd
Filip Macak <filip.macak1999@gmail.com>
2025-08-19 01:10:50 +0200

    permissive synthesis added

--- permissive.py
+++ permissive.py
@@ -4,7 +4,7 @@ import payntbind
 import stormpy
 
 import paynt.parser.sketch
-import paynt.synthesizer
+import paynt.synthesizer.synthesizer
 
 import os
 
@@ -12,17 +12,110 @@ import click
 import cProfile
 import pstats
 
+import logging
+logger = logging.getLogger(__name__)
 
+class PermissiveSynthesizer(paynt.synthesizer.synthesizer.Synthesizer):
 
-# class PermissiveSynthesizer:
+    @property
+    def method_name(self):
+        return "Permissive"
+    
+    def __init__(self, quotient):
+        super().__init__(quotient)
+        
+        self.permissive_policies = []
 
+    def check_specification(self, family):
+        ''' Check specification for mdp or smg based on self.quotient '''
+        mdp = family.mdp
 
+        if isinstance(self.quotient, paynt.quotient.posmg.PosmgQuotient):
+            model = self.quotient.create_smg_from_mdp(mdp)
+        else:
+            model = mdp
 
+        # check constraints
+        admissible_assignment = None
+        spec = self.quotient.specification
+        if family.constraint_indices is None:
+            family.constraint_indices = spec.all_constraint_indices()
+        results = [None for _ in spec.constraints]
+        for index in family.constraint_indices:
+            constraint = spec.constraints[index]
+            result = paynt.verification.property_result.MdpPropertyResult(constraint)
+            results[index] = result
 
+            # check primary direction
+            result.primary = model.model_check_property(constraint)
+            # print(f"primary: {result.primary.value}")
+            if result.primary.sat is False:
+                result.sat = False
+                break
 
+            result.primary_selection,_ = self.quotient.scheduler_is_consistent(mdp, constraint, result.primary.result)
 
+            # primary direction is SAT: check secondary direction to see whether all SAT
+            result.secondary = model.model_check_property(constraint, alt=True)
+            # print(f"secondary: {result.secondary.value}, {result.secondary.sat}")
+            if mdp.is_deterministic and result.primary.value != result.secondary.value:
+                logger.warning("WARNING: model is deterministic but min<max")
+            if result.secondary.sat:
+                result.sat = True
+                continue
 
+            primary_selection,_ = self.quotient.scheduler_is_consistent(mdp, constraint, result.primary.result)
+            secondary_selection,_ = self.quotient.scheduler_is_consistent(mdp, constraint, result.secondary.result)
 
+            assert len(primary_selection) == len(secondary_selection)
+            selection = [[] for _ in range(len(primary_selection))]
+            for i in range(len(primary_selection)):
+                selection[i] = primary_selection[i]
+                for x in secondary_selection[i]:
+                    if x not in selection[i]:
+                        selection[i].append(x)
+            result.primary_selection = selection
+
+        spec_result = paynt.verification.property_result.MdpSpecificationResult()
+        spec_result.constraints_result = paynt.verification.property_result.ConstraintsResult(results)
+
+        family.analysis_result = spec_result
+
+    def verify_family(self, family):
+        self.quotient.build(family)
+
+        # TODO include iteration_game in iteration? is it necessary?
+        if isinstance(self.quotient, paynt.quotient.posmg.PosmgQuotient):
+            self.stat.iteration_game(family.mdp.states)
+        else:
+            self.stat.iteration(family.mdp)
+
+        self.check_specification(family)
+
+    def check_result(self, family):
+        result = family.analysis_result
+        if result is None:
+            return
+        elif result.constraints_result.sat is False:
+            return
+        elif result.constraints_result.sat is True:
+            print(f"permissive scheduler found: {family.size}")
+            self.permissive_policies.append(family)
+
+    def synthesize_one(self, family):
+        families = [family]
+        while families:
+            if self.resource_limit_reached():
+                break
+            family = families.pop(-1)
+            self.verify_family(family)
+            self.check_result(family)
+            if family.analysis_result.constraints_result.sat is False or family.analysis_result.constraints_result.sat is True:
+                self.explore(family)
+                continue
+            # undecided
+            subfamilies = self.quotient.split(family)
+            families = families + subfamilies
 
 
 def print_profiler_stats(profiler):
@@ -80,11 +173,9 @@ def main(project, sketch, props, timeout, profiling):
 
     quotient = paynt.quotient.quotient.Quotient(explicit_quotient, family, coloring, specification)
 
-    # permissive_synthesizer = PermissiveSynthesizer(quotient)
+    permissive_synthesizer = PermissiveSynthesizer(quotient)
 
-    # print(f"{sys.argv[1]}, {quotient.quotient_mdp.nr_states}, {len(quotient.action_labels)}, {quotient.family.size}, {robust_policy_synthesizer.policy_family.size}, {quotient.specification.constraints[0].threshold}, , {robust_policy_synthesizer.game_abs_val}")
-
-    # permissive_synthesizer.run(synthesizer, timeout)
+    permissive_synthesizer.run()
 
     if profiling:
         profiler.disable()

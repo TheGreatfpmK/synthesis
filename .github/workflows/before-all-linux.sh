f3657dccaa95e189d399d6bac582f95669030c9d
Filip Macak <filip.macak1999@gmail.com>
2026-04-15 08:46:17 +0200

    testing nightly wheels for paynt

--- .github/workflows/before-all-linux.sh
+++ .github/workflows/before-all-linux.sh
@@ -18,18 +18,23 @@ make -j ${NR_JOBS}
 make install
 cd ..
 
-# Install Boost 1.83 from source
+# Install Boost 1.83 headers (no full build)
+#
+# Building all Boost libraries from source is extremely slow on GitHub-hosted Linux runners
+# and is unnecessary for our use (we only need Boost headers at >= 1.83 for compilation).
 BOOST_VERSION=1.83.0
 BOOST_VERSION_UNDERSCORE=${BOOST_VERSION//./_}
+BOOST_PREFIX=/opt/boost_${BOOST_VERSION_UNDERSCORE}
+
 curl -fsSLO https://archives.boost.io/release/${BOOST_VERSION}/source/boost_${BOOST_VERSION_UNDERSCORE}.tar.gz
 tar -xzf boost_${BOOST_VERSION_UNDERSCORE}.tar.gz
-cd boost_${BOOST_VERSION_UNDERSCORE}
-./bootstrap.sh --prefix=/usr/local
-./b2 install -j ${NR_JOBS}
-cd ..
+
+mkdir -p "${BOOST_PREFIX}/include"
+cp -a "boost_${BOOST_VERSION_UNDERSCORE}/boost" "${BOOST_PREFIX}/include/"
 
 # Install Storm
-git clone https://github.com/moves-rwth/storm.git -b master
+: "${STORM_VERSION:=master}"
+git clone --depth 1 https://github.com/moves-rwth/storm.git -b "${STORM_VERSION}"
 cd storm
 mkdir build
 cd build
--- .github/workflows/before-all-macos.sh
+++ .github/workflows/before-all-macos.sh
@@ -7,7 +7,8 @@ set -e -u
 brew install ccache automake boost cln ginac glpk hwloc z3 xerces-c
 
 # Install Storm
-git clone https://github.com/moves-rwth/storm.git -b master
+: "${STORM_VERSION:=master}"
+git clone --depth 1 https://github.com/moves-rwth/storm.git -b "${STORM_VERSION}"
 cd storm
 mkdir build
 cd build
